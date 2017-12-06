7649fffe18ef1e3dd94b2ff031da219e9f7e4ff2
stariel <ariel.altaras@gmail.com>
2017-12-05 21:50:14 -0800

    Working on read-on function, but it shows everything

--- ariel-rafael/scripts/articleView.js
+++ ariel-rafael/scripts/articleView.js
@@ -99,6 +99,13 @@ articleView.setTeasers = function() {
 
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
+
+  $('#articles').on('click', '.read-on', function(event) {
+    event.preventDefault();
+    $(this).prev().show();
+    // $('.article-body *:nth-of-type(n+2)').show();
+    $('.read-on').hide();
+  })
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
@@ -107,4 +114,5 @@ $(document).ready(function() {
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
+  articleView.setTeasers();
 })
